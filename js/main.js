c9e15589cea0a9b5b41deac4dfc51b22ea4911e4
Soloyolo <aruttigran@yandex.ru>
2021-03-25 18:53:33 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,28 +19,36 @@ const DIGIT_NUMBER = 5;
 
 const AD_QUANTITY = 10;
 
-/**
- * Получение случайного числа из диапазона
- * @param {number} min — минимальное значение
- * @param {number} max — максимальное значение
- * @param {number} digitsAfterDecpoint — количество знаков после запятой
- * @return {number} — случайное число
- */
-
-const getRandomNumberInRange = (min, max, digitsAfterDecpoint = 0) => {
-  if (min < 0) {
-    min = 0;
+// Функция, возвращающая случайное целое положительное число из переданного диапазона включительно
+const getRandomNumber =  (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // Ставим условие, что min положительное число, включая ноль, а также, что min всегда  либо больше, либо равен max.
+  if (min >= 0 && min <= max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-
-  if (max < 0) {
-    max = 0;
-  }
-
-  if (max < min) {
-    max = min;
-  }
-  return Number((min + Math.random() * (max - min)).toFixed(digitsAfterDecpoint));
+  // eslint-disable-next-line no-undef
+  throw new Error(INVALID_ARGUMENT);
 };
+
+getRandomNumber(1, 9);
+// Источник: https://developer.mozilla.org/
+
+
+// Функция, возвращающая случайное положительное число с заданным количеством цифр после запятой из переданного диапазона включительно
+const getRandomArbitrary = (min, max, digits) => {
+  // Ставим условие, что min положительное число, включая ноль, а также, что min всегда  либо больше, либо равен max.
+  if (min >= 0 && min <= max) {
+  // метод toFixed оставляет у полученного случайного числа заданное количество цифр после запятой
+    return (Math.random() * (max - min) + min).toFixed(digits);
+  }
+  // eslint-disable-next-line no-undef
+  throw new Error(INVALID_ARGUMENT);
+};
+
+getRandomArbitrary(1.1, 6.4, 2);
+// Источник: https://developer.mozilla.org/
+
 
 // СОЗДАЕМ МАССИВ ОБЪЯВЛЕНИЙ
 const getRandomArrayElement = (items) => {
