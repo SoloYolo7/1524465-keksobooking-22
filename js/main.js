0295c71ffceafb256572511dc955c3c6f14eecb7
Soloyolo <aruttigran@yandex.ru>
2021-03-25 19:00:42 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,107 +1,211 @@
-'use strict'
+'use strict';
 
-const [FIRST_ELEMENT, LAST_ELEMENT] = [1, 8];
-const [MIN_PRICE, MAX_PRICE] = [100, 10000];
-const TYPES = ['palace', 'flat', 'house', 'bungalow'];
-const [MIN_ROOMS, MAX_ROOMS] = [1, 8];
-const [MIN_QUESTS, MAX_QUESTS] = [1, 20];
-const TIME_EXAMPLES = ['12:00', '13:00', '14:00'];
-const TOTAL_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const TOTAL_PHOTOS = [
+const AVATARS_QUANTITY = 8;
+const CARDS_QUANTITY = 10;
+
+const coords = {
+  MIN_X: 35.65000,
+  MAX_X: 35.70000,
+  MIN_Y: 139.70000,
+  MAX_Y: 139.80000,
+  DIGITS: 5,
+};
+
+const titles = [
+  'Лучшее место отдохнуть от всех',
+  'Отличный выбор для двоих',
+  'Уединенное место для романтического уикенда',
+  'Места хватит даже для самой большой компании',
+  'Шикарные апартаменты для знающих себе цену людей',
+  'Рукой подать до всех главных достопримечательностей',
+  'Самый выгодный выбор за минимальные деньги',
+];
+
+const price = {
+  MIN: 0,
+  MAX: 1000000,
+};
+
+const accommodationTypes = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const roomsQuantity = {
+  MIN: 1,
+  MAX: 10,
+};
+
+const guestsQuantity = {
+  MIN: 1,
+  MAX: 50,
+};
+
+const checkinTimes = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const checkoutTimes = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const features = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const descriptions = [
+  'Комфортное и недорогое жилье в самом центре города. В пешей доступности находятся сразу несколько круглосуточных супермаркетов.',
+  'Жилье находится на тихой улочке. Со всех сторон разбиты красивые сады.',
+  'Жилье прямо у станции метро. Можно легко добраться до любого уголка города.',
+  'Уютное жилье для спокойного отдыха, вдали от шумных кварталов города.',
+  'Идеальный вариант для любителей шопинга: совсем рядом сразу несколько рынков и эксклюзивных бутиков.',
+  'Отличный вариант для семейного отдыха, с возможностью готовить еду на полностью оборудованной кухне.',
+  'Рядом находятся все самые популярные достопримечательности. Вам даже не придется тратить деньги на транспорт.',
+  'Из окон открывается прекрасный вид на центральный бульвар с пышной растительностью',
+];
+
+const photos = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
-const [X_MIN, X_MAX] = [35.65000, 35.70000];
-const [Y_MIN, Y_MAX] = [139.70000, 139.80000];
-const DIGIT_NUMBER = 5;
 
-const AD_QUANTITY = 10;
+/**
+ * Получение случайного числа из диапазона
+ * @param {number} min — минимальное значение
+ * @param {number} max — максимальное значение
+ * @param {number} digitsAfterDecpoint — количество знаков после запятой
+ * @return {number} — случайное число
+ */
 
-// Функция, возвращающая случайное целое положительное число из переданного диапазона включительно
-const getRandomNumber =  (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  // Ставим условие, что min положительное число, включая ноль, а также, что min всегда  либо больше, либо равен max.
-  if (min >= 0 && min <= max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomNumberInRange = (min, max, digitsAfterDecpoint = 0) => {
+  if (min < 0) {
+    min = 0;
   }
-  // eslint-disable-next-line no-undef
-  throw new Error(INVALID_ARGUMENT);
+
+  if (max < 0) {
+    max = 0;
+  }
+
+  if (max < min) {
+    max = min;
+  }
+  return Number((min + Math.random() * (max - min)).toFixed(digitsAfterDecpoint));
 };
 
-getRandomNumber(1, 9);
-// Источник: https://developer.mozilla.org/
 
+/**
+ * Перемешивание элементов массива в случайном порядке
+ * @param {array} array — исходный массив
+ * @return {array} — итоговый массив
+ */
 
-// Функция, возвращающая случайное положительное число с заданным количеством цифр после запятой из переданного диапазона включительно
-const getRandomArbitrary = (min, max, digits) => {
-  // Ставим условие, что min положительное число, включая ноль, а также, что min всегда  либо больше, либо равен max.
-  if (min >= 0 && min <= max) {
-  // метод toFixed оставляет у полученного случайного числа заданное количество цифр после запятой
-    return (Math.random() * (max - min) + min).toFixed(digits);
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
-  // eslint-disable-next-line no-undef
-  throw new Error(INVALID_ARGUMENT);
-};
-
-getRandomArbitrary(1.1, 6.4, 2);
-// Источник: https://developer.mozilla.org/
-
-
-// СОЗДАЕМ МАССИВ ОБЪЯВЛЕНИЙ
-const getRandomArrayElement = (items) => {
-  return items[getRandomIntInclusive(0, items.length - 1)];
+  return array;
 }
 
-// Перетасовка массива
-const shuffle = (items) => {
 
-  for (let i = items.length - 1; i > 0; i--) {
-    const j = getRandomIntInclusive(0, i);
-    [items[i], items[j]] = [items[j], items[i]];
-  }
+/**
+ * Получение случайного элемента массива
+ * @param {array} array — исходный массив
+ * @return {string|number|object} — значение массива со случайным индексом
+ */
 
-  return items;
+const getRandomArrayElement = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
-// Массив случайной длинны с неповторяющимися элементами
-const getArrayRandomLength = (items) => {
-  return shuffle(items.slice()).slice(0, getRandomIntInclusive(1, items.length));
+
+/**
+ * Получение случайного количества случайных элементов массива
+ * @param {array} array — исходный массив
+ * @return {array} — итоговый массив
+ */
+
+const getRandomQuantityArrayElements = (array) => {
+  return shuffleArray(array).slice(0, getRandomNumberInRange(1, array.length));
+}
+
+
+/**
+ * Получение случайного номера с добавлением незначащего нуля при необходимости
+ * @param {number} quantity — максимально возможное число - верхняя граница номера
+ * @return {number} — итоговое случайное число
+ */
+
+const getLeadingZeroRandomNumber = (quantity) => {
+  const number = getRandomNumberInRange(1, quantity);
+  const leadingZero = number < 10 ? '0' : '';
+  return leadingZero + number;
 };
 
-const createAd = () => {
-  const [xCoordinate, yCoordinate] = [
-    getRandomInclusive(X_MIN, X_MAX, DIGIT_NUMBER),
-    getRandomInclusive(Y_MIN, Y_MAX, DIGIT_NUMBER),
-  ];
 
-  return {
+/**
+ * Заполнение объявления пользователя мок-данными
+ * @return {object} — итоговый объект-объявление
+ */
+
+const renderCard = () => {
+  const COORD_X = getRandomNumberInRange(coords.MIN_X, coords.MAX_X, coords.DIGITS);
+  const COORD_Y = getRandomNumberInRange(coords.MIN_Y, coords.MAX_Y, coords.DIGITS);
+
+  const card = {
     author: {
-      avatar: 'img/avatars/user0' + getRandomIntInclusive(FIRST_ELEMENT, LAST_ELEMENT) + '.png',
+      avatar: 'img/avatars/user' + getLeadingZeroRandomNumber(AVATARS_QUANTITY) + '.png',
     },
-
     offer: {
-      title: 'Милая, уютная квартирка в центре Токио',
-      address: xCoordinate + ', ' + yCoordinate,
-      price: getRandomIntInclusive(MIN_PRICE, MAX_PRICE),
-      type: getRandomArrayElement(TYPES),
-      rooms: getRandomIntInclusive(MIN_ROOMS, MAX_ROOMS),
-      guests: getRandomIntInclusive(MIN_QUESTS, MAX_QUESTS),
-      checkin: getRandomArrayElement(TIME_EXAMPLES),
-      checkout: getRandomArrayElement(TIME_EXAMPLES),
-      features: getArrayRandomLength(TOTAL_FEATURES),
-      description: 'Небольшая уютная квартира',
-      photos: getArrayRandomLength(TOTAL_PHOTOS),
+      title: getRandomArrayElement(titles),
+      address: COORD_X + ', ' + COORD_Y,
+      price: getRandomNumberInRange(price.MIN, price.MAX),
+      type: getRandomArrayElement(accommodationTypes),
+      rooms: getRandomNumberInRange(roomsQuantity.MIN, roomsQuantity.MAX),
+      guests: getRandomNumberInRange(guestsQuantity.MIN, guestsQuantity.MAX),
+      checkin: checkinTimes[getRandomNumberInRange(0, checkinTimes.length - 1)],
+      checkout: checkoutTimes[getRandomNumberInRange(0, checkoutTimes.length - 1)],
+      features: getRandomQuantityArrayElements(features),
+      description: getRandomArrayElement(descriptions),
+      photos: getRandomQuantityArrayElements(photos),
     },
-
     location: {
-      x: xCoordinate,
-      y: yCoordinate,
+      x: COORD_X,
+      y: COORD_Y,
     },
-  }
+  };
+
+  return card;
 };
 
-const createAds = (quantity) => new Array(quantity).fill('').map(() => createAd());
-createAds(AD_QUANTITY);
+
+/**
+ * Создание массива из карточек-объявлений пользователя
+ * @param {number} quantity — количество карточек-объявлений
+ * @return {array} — итоговый массив из карточек
+ */
+
+const createCardsList = (quantity) => {
+  let cardsList = [];
+  for (let i = 0; i < quantity; i++) {
+    cardsList.push(renderCard(quantity));
+  }
+  return cardsList;
+}
+
+createCardsList(CARDS_QUANTITY);
